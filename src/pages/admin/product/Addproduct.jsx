41a9ed06b98e47e2e9c1ea137c6e8a1fd31079d0
milansir <rwn3developer11@gmail.com>
2024-02-16 08:58:02 +0530

    product-page-setup

--- src/pages/admin/product/Addproduct.jsx
+++ src/pages/admin/product/Addproduct.jsx
@@ -50,6 +50,34 @@ const Addproduct = () => {
                                     </div>
                             </div>
 
+                            <div className="col-lg-6">
+                                    <div className='p-4 shadow'>
+                                        <form>
+                                            <div className="form-group mb-3">
+                                                <label htmlFor="exampleInputEmail1">Category</label>
+                                                <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter category" />
+                                            </div>
+
+                                            <div className="form-group mb-3">
+                                                <label htmlFor="exampleInputEmail1">Product name</label>
+                                                <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter product" />
+                                            </div>
+
+                                            <div className="form-group mb-3">
+                                                <label htmlFor="exampleInputEmail1">Price</label>
+                                                <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter category" />
+                                            </div>
+
+                                            <div className="form-group mb-3">
+                                                <label htmlFor="exampleInputEmail1">Qty</label>
+                                                <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter category" />
+                                            </div>
+
+                                            <button type="submit" className="btn btn-primary">Submit</button>
+                                        </form>
+                                    </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
